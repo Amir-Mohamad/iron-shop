432a84a2a666b26f1396f17794c417a4b757bcb1
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-01-21 16:41:33 +0330

    new Changes

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def products_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category)
+    return render(request, 'core/products_list.html', {'products': products, 'categories': categories, 'category':category})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'core/product_detail.html', {'product': product})
