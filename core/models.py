25c591f3fdf1b679789fc97f9fcf4755a094f87d
Amir-Mohamad5 <y.amirmohamad8413@gmail.com>
2021-02-04 05:09:05 +0330

    Done - No Payment option

--- core/models.py
+++ core/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
